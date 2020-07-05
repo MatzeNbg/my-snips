2c858413f74f014cef30e33841f568dfc1182c54
Matze <66997348+MatzeNbg@users.noreply.github.com>
2020-07-05 13:55:52 +0200

    Update snips-install.sh

--- snips-install.sh
+++ snips-install.sh
@@ -2,4 +2,12 @@ sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y dirmngr
 sudo bash -c 'echo "deb https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
+sudo apt-key adv --keyserver gpg.mozilla.org --recv-keys D4F50CDCA10A2849
 sudo apt-get update
+sudo apt-get -y install git
+sudo apt-get install -y snips-platform-voice
+sudo apt-get install -y snips-template snips-skill-server
+cd ~
+git clone https://github.com/respeaker/seeed-voicecard
+cd seeed-voicecard
+sudo ./install.sh
